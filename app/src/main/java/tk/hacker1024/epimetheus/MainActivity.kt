aff24e2b96eb4345da6f884936bbffe9fd7dbf38
Unknown <joshleivenzon@outlook.com>
2018-10-05 12:56:46 +1000

    Change flags to restart activity with on logout

--- app/src/main/java/tk/hacker1024/epimetheus/MainActivity.kt
+++ app/src/main/java/tk/hacker1024/epimetheus/MainActivity.kt
@@ -77,7 +77,14 @@ class MainActivity : AppCompatActivity() {
                         R.id.logout -> {
                             getSharedPreferences(AUTH_SHARED_PREFS_NAME, Context.MODE_PRIVATE).edit().clear().apply()
                             finish()
-                            startActivity(Intent(this@MainActivity, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+                            startActivity(
+                                Intent(this@MainActivity, MainActivity::class.java)
+                                    .addFlags(
+                                        Intent.FLAG_ACTIVITY_NO_ANIMATION or
+                                        Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                                        Intent.FLAG_ACTIVITY_NEW_TASK
+                                    )
+                            )
                             true
                         }
                         else -> false
